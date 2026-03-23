9c3ee7e67d00df59405d90598b490ba70a226ae7
hiramatsutko <hiramatsutko@jobcan.biz>
2026-03-23 17:04:24 +0900

    js勉強コード

---
+++ nodescript/index.js
@@ -0,0 +1,12 @@
+function printName() {
+    setTimeout(() => {
+        console.log(this.name);
+    }, 1000);
+}
+
+const obj = {
+    name: "John Doe",
+    printName: printName,
+};
+
+obj.printName();
