9ca07db3bf11d0ab477bce4783bbe6ba2dedef42
takao827 <takao827.dds@gmail.com>
2026-03-24 23:45:02 +0000

    eslint追加

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+require('eslint/config');
+const js = require('@eslint/js');
+const globals = require('globals');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      semi: ['error', 'always', { 'omitLastInOneLineBlock': true }],
+      quotes: ['error', 'single'],
+    },
+  }
+];
