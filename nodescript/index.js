500e47f423171855ed6511a801994d4ccf3fe74e
takao827 <takao827.dds@gmail.com>
2026-03-24 00:01:15 +0000

    インデント幅変更(4->2)

--- nodescript/index.js
+++ nodescript/index.js
@@ -1,12 +1,12 @@
 function printName() {
-    setTimeout(() => {
-        console.log(this.name);
-    }, 1000);
+  setTimeout(() => {
+    console.log(this.name);
+  }, 1000);
 }
 
 const obj = {
-    name: "John Doe",
-    printName: printName,
+  name: "John Doe",
+  printName: printName,
 };
 
 obj.printName();
