60b2fc5ece2fa8a1a7920b97470d11125ea17bfc
takao827 <takao827.dds@gmail.com>
2026-04-21 00:05:58 +0000

    Next.jsインストール

--- packages/frontend/src/components/Hello.tsx
+++
@@ -1,9 +0,0 @@
-const Hello = () => {
-  const onClick = () => {
-    alert('hello');
-  };
-  const text = 'hello, react';
-  return <div onClick={onClick}>{text}</div>;
-};
-
-export default Hello;
--- packages/frontend/src/components/Text.tsx
+++
@@ -1,6 +0,0 @@
-const Text = (props: { content: string }) => {
-  const { content } = props;
-  return <p className="text">{content}</p>;
-};
-
-export default Text;
