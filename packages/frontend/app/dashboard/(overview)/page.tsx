d039d3bd064d96e7bab500ff7fed83a912bff15e
takao827 <takao827.dds@gmail.com>
2026-04-29 06:07:41 +0000

    App Router C15 [Adding Metadata]

--- packages/frontend/app/dashboard/(overview)/page.tsx
+++ packages/frontend/app/dashboard/(overview)/page.tsx
@@ -8,6 +8,11 @@ import {
   RevenueChartSkeleton,
   LatestInvoicesSkeleton,
 } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Home',
+};
 
 export default async function Page() {
   return (
