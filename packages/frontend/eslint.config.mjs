af9a016f2ce36f99ee03ea03a2c5c0bb0dd5d324
takao827 <takao827.dds@gmail.com>
2026-05-03 17:14:50 +0000

    設定修正 #4

--- packages/frontend/eslint.config.mjs
+++ packages/frontend/eslint.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig, globalIgnores } from 'eslint/config';
+import { defineConfig, globalIgnores } from '@eslint/js';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 
 const eslintConfig = defineConfig([
